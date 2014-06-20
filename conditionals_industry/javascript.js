53ead777afdfaca7c0b704e724b8270fc3813f40
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 17:28:37 -0700

    labels for first part of the equation.
    labels for first part of the equation.

--- conditionals_industry/javascript.js
+++ conditionals_industry/javascript.js
@@ -1,13 +1,14 @@
 //duties today
 
-var workDuty= prompt("today am I driving or working in the warehouse");
+var workDuty= prompt("today am I driving or working in the warehouse");//what work duties am I expected to preform today?
 
-if(workDuty==="")prompt("enter a reply fo driving or working in the warehouse today ");
+if(workDuty==="")prompt("enter a reply fo driving or working in the warehouse today ");//validating reply for prompt.
 
-if(workDuty==="driving"){alert("I need to be at work by 4a.m.");
+if(workDuty==="driving"){alert("I need to be at work by 4a.m.");//if driving then i need to start my day at 4am.
 
 }else{
 
     alert("need to be at work at 6a.m.");
 
-}
+}//conditional phrase to decide if I start work at 4am or 6am
+
