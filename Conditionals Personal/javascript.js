d45e799aaab9f5d12b2f61fe0ac83da1635ef963
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 17:05:20 -0700

    labels for equation
    labels for equation.

--- Conditionals Personal/javascript.js
+++ Conditionals Personal/javascript.js
@@ -8,16 +8,16 @@ var userInput =prompt("what day is it?");//user input
 var workDays= ["monday" , "tuesday" , "wednesday" , "thursday" , "friday"]; //days that I work monday-friday
 
 
-var dayOff=["saturday","sunday"];
+var dayOff=["saturday","sunday"];//days off variable
 
-if ( userInput=== ("")) prompt("enter a day monday-sunday") ;
+if ( userInput=== ("")) prompt("enter a day monday-sunday");//validate the prompt.
 
 console.log(userInput);//log of input.
 
 
-if(userInput=workDays){alert("I work today");
+if(userInput=workDays){alert("I work today");//if user input is equal to work day then use alert.
 
-}else
+}else//if user input is equal to day off variable use other alert.
 
 {(userInput=dayOff);alert("I do not work today!");
 
