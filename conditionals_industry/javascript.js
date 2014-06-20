c1b2b36704b80092d58d8a4728807bda1de655fd
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 17:41:17 -0700

    second equation.
    second equation. Also had to but the starting variable back into the
    validation prompt or would get the wrong answer.

--- conditionals_industry/javascript.js
+++ conditionals_industry/javascript.js
@@ -2,7 +2,7 @@
 
 var workDuty= prompt("today am I driving or working in the warehouse");//what work duties am I expected to preform today?
 
-if(workDuty==="")prompt("enter a reply fo driving or working in the warehouse today ");//validating reply for prompt.
+if(workDuty==="")var workDuty= prompt("enter a reply for driving or working in the warehouse today ");//validating reply for prompt.
 
 if(workDuty==="driving"){alert("I need to be at work by 4a.m.");//if driving then i need to start my day at 4am.
 
@@ -12,3 +12,13 @@ if(workDuty==="driving"){alert("I need to be at work by 4a.m.");//if driving the
 
 }//conditional phrase to decide if I start work at 4am or 6am
 
+var startTime= prompt("what time was given to start work?");
+
+if(startTime==="")var startTime= prompt("enter your given start time for work as just a number no suffix");
+
+if(startTime==="4"){alert("I will be off work at noon");
+
+}else{
+
+    alert("I will be off work at 2pm");
+}
