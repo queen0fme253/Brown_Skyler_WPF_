f663b65c93e9d310756b4ec46fc80d8ecc4640e6
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 18:28:10 -0700

    created parameters and added alert
    created the parameter to defeat my lizard and added an alert to show
    results.

--- Whacky_Conditional/javascript.js
+++ Whacky_Conditional/javascript.js
@@ -4,11 +4,16 @@
 var slaying= true;//boolean for slaying Trogdor.
 var youHit= Math.floor(Math.random()*2);//varible for striking.
 var damageThisRound= Math.floor(Math.random()*5+1);//variable for damage this round.
+
 var totalDamage=0;//added to track damage through simulation.
 
 while(slaying){
     if(youHit){
         console.log("You hit my mighty lizard and did "+damageThisRound+ " damage!");
+totalDamage += damageThisRound;
+        if(totalDamage >=4){
+            alert("You defeated Trogdor! you owe me a new lizard!");
 
-    }
+            
+        }    }
 }
