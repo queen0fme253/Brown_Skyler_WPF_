6976b2aa55f3013aac0cdcb3e1a6942672f2e78e
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 17:01:50 -0700

    equation
    equation

--- Conditionals Personal/javascript.js
+++ Conditionals Personal/javascript.js
@@ -1,4 +1,30 @@
-alert("this program will check imputs of user to see if I work on day imputed");
+alert("this program will check inputs of user to see if I work on day imputed");
 
-var workdays= ["monday","tuesday","wednesday","thursday","friday"]; //days that I work
+
+
+var userInput =prompt("what day is it?");//user input
+
+
+var workDays= ["monday" , "tuesday" , "wednesday" , "thursday" , "friday"]; //days that I work monday-friday
+
+
+var dayOff=["saturday","sunday"];
+
+if ( userInput=== ("")) prompt("enter a day monday-sunday") ;
+
+console.log(userInput);//log of input.
+
+
+if(userInput=workDays){alert("I work today");
+
+}else
+
+{(userInput=dayOff);alert("I do not work today!");
+
+
+}
+
+
+
+
 
