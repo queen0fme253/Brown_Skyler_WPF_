03a3b21c5d44506e45e78006a68953743c8fe1ac
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 17:25:13 -0700

    basic first equation for industry conditional.
    basic first equation for industry conditional.

--- conditionals_industry/javascript.js
+++ conditionals_industry/javascript.js
@@ -1,1 +1,13 @@
+//duties today
 
+var workDuty= prompt("today am I driving or working in the warehouse");
+
+if(workDuty==="")prompt("enter a reply fo driving or working in the warehouse today ");
+
+if(workDuty==="driving"){alert("I need to be at work by 4a.m.");
+
+}else{
+
+    alert("need to be at work at 6a.m.");
+
+}
