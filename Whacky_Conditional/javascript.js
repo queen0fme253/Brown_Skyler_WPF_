fabbe0bf852a5610b4f7487467f54b9acd3d57a3
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 18:35:48 -0700

    Added second conditional
    Added a second conditional if slaying=false.

--- Whacky_Conditional/javascript.js
+++ Whacky_Conditional/javascript.js
@@ -11,9 +11,18 @@ while(slaying){
     if(youHit){
         console.log("You hit my mighty lizard and did "+damageThisRound+ " damage!");
 totalDamage += damageThisRound;
-        if(totalDamage >=4){
+        if(totalDamage >=4) {
             alert("You defeated Trogdor! you owe me a new lizard!");
 
-            
-        }    }
+            slaying = false;
+
+        }else{
+
+        youHit=Math.floor(Math.random()*2);
+        }
+    }{
+    }
 }
+
+
+
