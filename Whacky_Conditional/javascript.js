1eb3ca5a46ba14dc4d759095ef465ca0c02be9fc
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 18:11:27 -0700

    Variables
    variables needed to simulate.

--- Whacky_Conditional/javascript.js
+++ Whacky_Conditional/javascript.js
@@ -1,1 +1,6 @@
 //battle Trogdor using math.
+
+
+var slaying= true;//boolean for slaying Trogdor.
+var youHit= Math.floor(Math.random()*2);//varible for striking.
+var damageThisRound= Math.floor(Math.random()*5+1);//variable for damage this round.
