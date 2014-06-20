d4dfc0e8da3c0e9abece89ae14f4c2b5e5c88a8b
sbrown7861 <sbrown14@fullsail.edu>
2014-06-19 18:18:07 -0700

    console.log for first part of first conditional.
    console.log for first part of first conditional.

--- Whacky_Conditional/javascript.js
+++ Whacky_Conditional/javascript.js
@@ -4,3 +4,11 @@
 var slaying= true;//boolean for slaying Trogdor.
 var youHit= Math.floor(Math.random()*2);//varible for striking.
 var damageThisRound= Math.floor(Math.random()*5+1);//variable for damage this round.
+
+
+while(slaying){
+    if(youHit){
+        console.log("You hit my mighty lizzard and did "+damageThisRound+ " damage!");
+        
+    }
+}
