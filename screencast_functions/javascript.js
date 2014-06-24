d8bd81bdb4fe3e820984a7fd6bd286f796b0f21b
sbrown7861 <sbrown14@fullsail.edu>
2014-06-23 20:46:38 -0700

    screencast functions
    screencast functions

---
+++ screencast_functions/javascript.js
@@ -0,0 +1,18 @@
+
+
+
+ calcArea(30,20);
+
+function calcArea(w,h){
+
+    area= w*h;
+    console.log(area)
+
+}
+
+ var total=calcArea1(30,15);
+ function calcArea1(w,h){
+     area1=(w*h);
+     return area1;
+ }
+ console.log(total);
