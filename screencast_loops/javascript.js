1b61ffedc2749a05d656d5c5e8eb82034e476076
sbrown7861 <sbrown14@fullsail.edu>
2014-06-24 19:56:19 -0700

    loops screen cast.
    loops screen cast.

---
+++ screencast_loops/javascript.js
@@ -0,0 +1,22 @@
+var b= 10;  //while loop setup
+if(b>0){
+    console.log (b)
+
+}
+
+while (b>0){
+    console.log (b+" kegs on the wall");
+    b--;
+}
+
+
+var c=10;   //do while loop
+do{
+    console.log(c+ " kegs on the wall");
+    c--;
+}
+while(c>0);
+
+for(var i=10; i>0; i--){    //for loop
+    console.log(i +" kegs on the wall")
+}
