372a010a226bd37ddf4bb03c8336a3177a70c421
sbrown7861 <sbrown14@fullsail.edu>
2014-06-26 16:04:17 -0700

    Second variable and validation loop.
    Second variable and validation loop.

--- functions_personal/javascript.js
+++ functions_personal/javascript.js
@@ -5,3 +5,7 @@ while(loan===""){
     loan=prompt("do not leave blank, please enter a number value.");//validation loop for var loan.
 }
 
+var time=prompt("How many years until your first payment occurs?");//variable for time.
+while(time===""){
+    loan=prompt("Do not leave blank, enter years until first payment occurs.");//validation loop for var time.
+}
