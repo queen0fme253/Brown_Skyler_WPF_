df98b92d056637a28b541e32bda0cf93eec6500e
sbrown7861 <sbrown14@fullsail.edu>
2014-06-26 16:00:49 -0700

    First variable and validation.
    First variable and validation for that variable. While loops are
    awesome.

---
+++ functions_personal/javascript.js
@@ -0,0 +1,7 @@
+//calculate gained interest on student loans before repayment begins.
+
+var loan=prompt("what is the loan amount of the student loan?");//variable for student loan amount.
+while(loan===""){
+    loan=prompt("do not leave blank, please enter a number value.");//validation loop for var loan.
+}
+
