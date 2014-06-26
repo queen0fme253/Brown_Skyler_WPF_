6653240e03c58c680bccb57da398249c830ec0ca
sbrown7861 <sbrown14@fullsail.edu>
2014-06-26 16:51:22 -0700

    First variable and validation.
    First variable and validation, for industry functions.

--- Functions_industry/javascript.js
+++ Functions_industry/javascript.js
@@ -1,2 +1,6 @@
 //I work in an electrical warehouse so I figured this would be a fun one to do.
 
+var p=prompt("How many watts are being used");//First variable is power measured in watts.
+while(p===""){
+    p=prompt("Give a number value for watts, do not leave blank");//validation loop for var p
+}
