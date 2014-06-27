bec8ee79500a349f49d7957e14e263fa3ee6a037
sbrown7861 <sbrown14@fullsail.edu>
2014-06-26 17:18:07 -0700

    Added variables for BMI
    Added a variable and a validation loop for height and weight.

--- Whacky_functions/javascript.js
+++ Whacky_functions/javascript.js
@@ -1,1 +1,10 @@
 //Javascript code to calculate BMI
+var height=prompt("Enter your height in inches.");//variable for height in inches
+while(height===""){
+    height=prompt("Enter your height in inches, do not leave blank");//validation prompt for height in inches.
+}
+
+var weight=prompt("Enter your weight in pounds");//variable for weight
+while(height===""){
+    weight=prompt("Enter your weight in pounds, do not leave blank ");
+}//validation loop for weight.
