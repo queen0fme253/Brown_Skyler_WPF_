1a0354a6d7321e0b329c284faff8c39719376466
sbrown7861 <sbrown14@fullsail.edu>
2014-06-26 17:25:49 -0700

    Function for BMI
    Function and console.log for BMI

--- Whacky_functions/javascript.js
+++ Whacky_functions/javascript.js
@@ -8,3 +8,11 @@ var weight=prompt("Enter your weight in pounds");//variable for weight
 while(height===""){
     weight=prompt("Enter your weight in pounds, do not leave blank ");
 }//validation loop for weight.
+console.log("your height is "+height+" inches");
+console.log("your weight is "+weight+" pounds");
+
+function BMI(height,weight){
+    BMI=weight/(height/100*height/100);
+    console.log("your BMI is "+BMI);
+    return BMI;
+}//Function and console.log for BMI
